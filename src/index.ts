429d956d5e8f5587d4e5a20a038b028f61bf5275
uros-simeunovic <dixypixy007@gmail.com>
2024-03-17 15:47:05 +0100

    import problem

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import cors from "cors";
+const express = require("express");
+const cors = require("cors");
 // import ObjectRoute from "./routes/ObjectRoute";
 
 const app = express();
